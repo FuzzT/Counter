438f378f10c377c416ae206fbddf6971f03420d4
Abhay sagar kargeti <94851856+FuzzT@users.noreply.github.com>
2022-06-21 13:09:30 +0530

    Add files via upload

---
+++ CounterApp/index.js
@@ -0,0 +1,17 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (x) {
+    const styles = x.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
